5a0975a6078ca2a65a437b60e11453f2076fa6ff
juancartagena618 <jcsamc@gmail.com>
2023-10-16 14:21:49 -0400

    random number

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - main block
- * description: random number
- * return 0
+ * main - Entry point
+ * Description: 'random numbe and print number'
+ * Return: Always 0
  */
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >0)
+	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
