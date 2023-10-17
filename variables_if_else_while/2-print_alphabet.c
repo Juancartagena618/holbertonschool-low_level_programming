83b563c0eec5b86ac6a0ccd81818fd4779b73281
juancartagena618 <jcsamc@gmail.com>
2023-10-17 10:37:24 -0400

    lowercase_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - 'Print alphabet in lowercase'
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+	}
+
+	putchar('\n');
+	return (0);
+}
