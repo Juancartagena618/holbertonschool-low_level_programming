dfc2a1329db11829972a530c24d7012c98979f1c
juancartagena618 <jcsamc@gmail.com>
2023-10-17 13:29:13 -0400

    3-print_alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,17 +5,20 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
 	char c;
 
 	for (c = 'a' ; c <= 'z'; c++)
+	{
 		putchar (c);
+	}
 
 	for (c = 'A' ; c <= 'Z'; c++)
+	{
 		putchar (c);
+	}
 
-	putchar('n\');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
