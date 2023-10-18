973f20cb8ed4f319bdf814bdb1ea0c8b68747ec8
juancartagena618 <jcsamc@gmail.com>
2023-10-18 13:44:12 -0400

    print_alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints tha alphabet, in lowercase followed by new line
+ * Return:0
+ */
+
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
