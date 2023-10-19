cc4bb4fe11ab6010ccf5bec73bc9772c002bcc31
juancartagena618 <jcsamc@gmail.com>
2023-10-19 09:40:38 -0400

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _islower - funtion that checks for lowercase character
+ * @c: character to check
+ * Return: 0
+ */
+int _islower(int c);
+{
+		if (c >= 'a' && c <= 'z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
